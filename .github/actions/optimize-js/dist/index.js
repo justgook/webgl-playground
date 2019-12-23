5d874cde56c860108747184e97b8760ba8d88aac
Romans Potasovs <justgook@gmail.com>
2019-12-24 00:51:25 +0200

    Try build multiple files (attempt 5)

--- .github/actions/optimize-js/dist/index.js
+++ .github/actions/optimize-js/dist/index.js
@@ -345,12 +345,24 @@ module.exports = require("path");
 const { exec } = __webpack_require__(129);
 const core = __webpack_require__(294);
 
+
+const cmd = (aaa) => exec(aaa, (err, stdout, stderr) => {
+    if (err) {
+        // node couldn't execute the command
+        return -1;
+    }
+
+    // the *entire* stdout and stderr (buffered)
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+});
+
 const inputFile = core.getInput('elm-version');
 
-exec(`jscodeshift -t transform.js ${inputFile}`);
-exec(`uglifyjs ${inputFile} --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters" --output=${inputFile}`);
-exec(`prepack ${inputFile} --inlineExpressions --out ${inputFile} --maxStackDepth  10000`);
-exec(`uglifyjs ${inputFile} --compress 'keep_fargs=false,unsafe_comps,unsafe' --mangle --output=${inputFile}`);
+// cmd(`jscodeshift -t transform.js ${inputFile}`);
+cmd(`uglifyjs ${inputFile} --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters" --output=${inputFile}`);
+// cmd(`prepack ${inputFile} --inlineExpressions --out ${inputFile} --maxStackDepth  10000`);
+// cmd(`uglifyjs ${inputFile} --compress 'keep_fargs=false,unsafe_comps,unsafe' --mangle --output=${inputFile}`);
 
 
 /***/ })
--- .github/actions/optimize-js/index.js
+++ .github/actions/optimize-js/index.js
@@ -15,7 +15,7 @@ const cmd = (aaa) => exec(aaa, (err, stdout, stderr) => {
 
 const inputFile = core.getInput('elm-version');
 
-cmd(`jscodeshift -t transform.js ${inputFile}`);
+// cmd(`jscodeshift -t transform.js ${inputFile}`);
 cmd(`uglifyjs ${inputFile} --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters" --output=${inputFile}`);
-cmd(`prepack ${inputFile} --inlineExpressions --out ${inputFile} --maxStackDepth  10000`);
-cmd(`uglifyjs ${inputFile} --compress 'keep_fargs=false,unsafe_comps,unsafe' --mangle --output=${inputFile}`);
+// cmd(`prepack ${inputFile} --inlineExpressions --out ${inputFile} --maxStackDepth  10000`);
+// cmd(`uglifyjs ${inputFile} --compress 'keep_fargs=false,unsafe_comps,unsafe' --mangle --output=${inputFile}`);
